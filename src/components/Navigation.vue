7171ec46ca19108327255af298ab8b5309ee001b
cnotv <contact@cnotv.xyz>
2018-10-07 18:03:27 +0200

    Add Vuetify; Set navbar icons

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,9 +1,29 @@
 <template>
-	<div id="nav">
-		<router-link to="/">Dashboard</router-link> |
-		<router-link to="/farms">farms</router-link> |
-		<router-link to="/diseases">diseases</router-link> |
-		<router-link to="/symptoms">symptoms</router-link> |
-		<router-link to="/cases">cases</router-link>
-	</div>
+  <v-toolbar color="primary">
+    <v-toolbar-side-icon></v-toolbar-side-icon>
+
+    <v-toolbar-title>Title</v-toolbar-title>
+
+    <v-spacer></v-spacer>
+
+		<v-btn to="/" icon>
+			<v-icon>fa-chart-line</v-icon>
+		</v-btn>
+
+		<v-btn to="/farms" icon>
+			<v-icon>fa-pills</v-icon>
+		</v-btn>
+
+		<v-btn to="/diseases" icon>
+			<v-icon>fa-bug</v-icon>
+		</v-btn>
+
+		<v-btn to="/symptoms" icon>
+			<v-icon>fa-dizzy</v-icon>
+		</v-btn>
+
+		<v-btn to="/cases" icon>
+			<v-icon>fa-heartbeat</v-icon>
+		</v-btn>
+  </v-toolbar>
 </template>
