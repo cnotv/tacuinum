e4aaa8f5160713e768276c847cbd41c7f87b1e5c
cnotv <contact@cnotv.xyz>
2019-01-28 10:49:05 +0100

    Add Sentry to the project

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,17 @@ import './plugins/vuetify'
 import App from './App.vue'
 import store from './store'
 import router from './router'
+import * as Sentry from '@sentry/browser'
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import '@fortawesome/fontawesome-free/css/all.css'
 import './registerServiceWorker'
 
 Vue.config.productionTip = false
+
+Sentry.init({
+  dsn: 'https://22553bd0e4cf47d487248ac2547e8047@sentry.io/1379644',
+  integrations: [new Sentry.Integrations.Vue({ Vue })]
+})
 
 new Vue({
   store,
